c47103950e1d960428757c976b0a785521fde0f0
Hozy <kiprutohosea05@gmail.com>
2025-06-17 04:17:53 +0300

    Added the Categories Card and Hardcoded the categories data

--- app/src/main/java/com/learn/ecommersapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/learn/ecommersapp/screens/home/HomeScreen.kt
@@ -1,18 +1,21 @@
 package com.learn.ecommersapp.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import com.learn.ecommersapp.model.Category
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -40,6 +43,33 @@ fun HomeScreen(){
             //Categories Section
             SectionTitle("Categories", "See All"){
                 /* TODO() add actions  */
+            }
+
+            //Moke the categories Data
+
+            val categories: List<Category> = listOf(
+                Category(1,"Electronics","https://www.flaticon.com/free-icon/device_8990736"),
+
+                Category(2,"Clothing","https://www.flaticon.com/free-icon/laundry_2946537")
+
+            )
+            val selectedCategory=remember { mutableStateOf(0) }
+
+            //
+
+            LazyRow(
+                contentPadding = PaddingValues (horizontal=16.dp),
+                horizontalArrangement = Arrangement.spacedBy (8.dp)) {
+                items (categories.size){
+                    CategoriesChip(
+                        icon=categories[it].iconUrl,
+                        text=categories[it].name,
+                        isSelected = selectedCategory.value==it,
+                        onClick ={
+                            selectedCategory.value=it
+                            /*Do some search logig*/                        }
+                    )
+                }
             }
 
             // Featured Products section
--- app/src/main/java/com/learn/ecommersapp/ui/theme/Color.kt
+++ app/src/main/java/com/learn/ecommersapp/ui/theme/Color.kt
@@ -11,3 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val PrimaryColor =Color(0xFF6650a4)
+val CardChipColor=Color(0xFF00FF00)
